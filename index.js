b7a005260cdea695df0a4719fe1a3395c6bc4741
BuildTools <unconfigured@null.spigotmc.org>
2019-03-08 10:49:34 -0800

    heroku test

--- index.js
+++ index.js
@@ -1,21 +1,9 @@
 const express = require("express");
-const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const keys = require("./config/keys.js");
 const app = express();
 
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: keys.googleClientID,
-      clientSecret: keys.googleClientSecret,
-      callbackURL: "/auth/google/callback"
-    },
-    accessToken => {
-      console.log(accessToken);
-    }
-  )
-);
+app.get("/", (req, res) => {
+  res.send({ hi: "there" });
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
